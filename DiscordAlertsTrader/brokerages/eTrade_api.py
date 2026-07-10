7e15f3bde13c3b123f96aba386a69c09be2f6a75
Nadir Ali Khan <124476721+NadirAliOfficial@users.noreply.github.com>
2026-07-10 15:39:53 +0500

    fix: remove trailing whitespace

--- DiscordAlertsTrader/brokerages/eTrade_api.py
+++ DiscordAlertsTrader/brokerages/eTrade_api.py
@@ -22,7 +22,7 @@ def retry_on_exception(retries=3, do_raise=False, fallback_method=None):
                     return func(*args, **kwargs)
                 except Exception as e:
                     print(f"Exception occurred: {e}. Retrying... (Attempt {attempt}/{retries})")
-            
+
             # if fallback_method:
             #     try:
             #         fallback_method()
@@ -45,7 +45,7 @@ class eTrade(BaseBroker):
         self.consumer_key = cfg["etrade"]["CONSUMER_KEY"]
         self.consumer_secret = cfg["etrade"]["CONSUMER_SECRET"]
         self.token_fname = os.path.join(cfg['root']['dir'], "tokens.json")
-        
+
     def get_session(self):
         """get token and sessions, will try several times and sleep for a second between each try"""
         for ix in range(5):
@@ -57,11 +57,11 @@ class eTrade(BaseBroker):
         raise Exception("Could not get session")
 
     def _get_access_token(self, oauth,verifier_code):
-        """Gets access token and tries 3 times before giving up"""        
+        """Gets access token and tries 3 times before giving up"""
         for ix in range(3):
             try:
                 request_token = oauth.get_access_token(verifier_code)
-                return request_token                
+                return request_token
             except Exception:
                 print(f"Could not get token, trying again {ix}/3")
                 time.sleep(1)
@@ -79,24 +79,24 @@ class eTrade(BaseBroker):
                 'dev': False
                 }
             self.account_session = pyetrade.ETradeAccounts(**kwargs)
-            self.market_session = pyetrade.ETradeMarket(**kwargs)     
+            self.market_session = pyetrade.ETradeMarket(**kwargs)
             self.order_session = pyetrade.ETradeOrder(**kwargs)
             kwargs.pop('dev')
             self.accessmanager = pyetrade.ETradeAccessManager(**kwargs)
             self.accessmanager.renew_access_token()
             self._get_account()
             return True
-        
+
         # if tokens saved try getting session
         if os.path.exists(self.token_fname):
             with open(self.token_fname, "r") as f:
-                self.tokens = json.load(f)   
+                self.tokens = json.load(f)
             try:
-                return sessions()  
+                return sessions()
             except Exception:
                 print("Loaded tokens expired, requesting new tokens")
-                os.remove(self.token_fname)  
-        
+                os.remove(self.token_fname)
+
         # if tokens not valid, get new ones
         oauth = pyetrade.ETradeOAuth(self.consumer_key, self.consumer_secret)
         if cfg['etrade'].getboolean('WITH_BROWSER'):
@@ -108,22 +108,22 @@ class eTrade(BaseBroker):
         self.tokens = self._get_access_token(oauth, verifier_code)
         with open(self.token_fname, "w") as f:
             json.dump(self.tokens, f)
-        return sessions() 
+        return sessions()
 
     def renew_access(self):
         """Renews access token"""
         self.accessmanager.renew_access_token()
-        
+
     @retry_on_exception()
     def _get_account(self):
         """
         Calls account list API to retrieve a list of the user's E*TRADE accounts
         """
         data = self.account_session.list_accounts(resp_format='json')
-        self.accounts_list = data["AccountListResponse"]["Accounts"]["Account"]        
-        
+        self.accounts_list = data["AccountListResponse"]["Accounts"]["Account"]
+
         if self.accountId is not None:
-            self.accountIdKey = [self.accounts_list[i]['accountIdKey'] for i in range(len(self.accounts_list)) 
+            self.accountIdKey = [self.accounts_list[i]['accountIdKey'] for i in range(len(self.accounts_list))
                                  if self.accounts_list[i]['accountId'] == self.accountId][0]
         else:
             self.accountIdKey = self.accounts_list[self.account_n]['accountIdKey']
@@ -135,9 +135,9 @@ class eTrade(BaseBroker):
         """
         Call portfolio API to retrieve a list of positions held in the specified account
         """
-        data = self.account_session.get_account_balance(self.accountIdKey, resp_format='json') 
+        data = self.account_session.get_account_balance(self.accountIdKey, resp_format='json')
         acc_inf ={
-            'securitiesAccount':{   
+            'securitiesAccount':{
                 'positions':[],
                 'accountId' : self.accountId,
                 'currentBalances':{
@@ -146,7 +146,7 @@ class eTrade(BaseBroker):
                     'availableFunds': data['BalanceResponse'].get("Computed").get('cashAvailableForInvestment'),
                     },
         }}
-        
+
         # get orders and add them to acc_inf
         orders = self.get_orders()
         orders_inf =[]
@@ -156,11 +156,11 @@ class eTrade(BaseBroker):
                 continue
             orders_inf.append(self.format_order(order))
         acc_inf['securitiesAccount']['orderStrategies'] = orders_inf
-        
+
         try:
             data = self.account_session.get_account_portfolio(self.accountIdKey, resp_format='json')
         except Exception:
-            return acc_inf    
+            return acc_inf
         # Handle and parse response
         if data is not None and "PortfolioResponse" in data and "AccountPortfolio" in data["PortfolioResponse"]:
             for acctPortfolio in data["PortfolioResponse"]["AccountPortfolio"]:
@@ -182,7 +182,7 @@ class eTrade(BaseBroker):
                         acc_inf['securitiesAccount']['positions'].append(pos)
         else:
             print("No portfolio")
-            
+
         return acc_inf
 
     def get_positions_orders(self):
@@ -214,7 +214,7 @@ class eTrade(BaseBroker):
     def format_option(self, opt_ticker:str)->str:
         """From ticker_monthdayyear[callput]strike to ticker:year:month:day:optionType:strikePrice"""
 
-        exp = r"(\w+)_(\d{2})(\d{2})(\d{2})([CP])([\d.]+)"        
+        exp = r"(\w+)_(\d{2})(\d{2})(\d{2})([CP])([\d.]+)"
         match = re.search(exp, opt_ticker, re.IGNORECASE)
         if match:
             symbol, mnt, day, yer, type, strike = match.groups()
@@ -235,7 +235,7 @@ class eTrade(BaseBroker):
         # reformat option tickers
 
         symbol = [self.format_option(i) if "_" in i else i for i in symbol ]
-        
+
         resp = {}
         for ix in range(0,len(symbol),25):  # lim is 25, loop over 25 symbols at a time
             symbol_l = symbol[ix:ix+25]
@@ -253,19 +253,19 @@ class eTrade(BaseBroker):
                     for quote in data["QuoteResponse"]["QuoteData"]:
                         if quote is None:
                             resp[ticker] = {'description': 'Symbol not found'}
-                            continue 
+                            continue
                         if  quote.get("Product").get('securityType') == "OPTN":
                             # to common format ticker_monthdayyearstrike[callput]
                             prod = quote['Product']
                             opty = prod['callPut'][0].upper().replace('CALL','C').replace('PUT','P')
                             ticker = f"{prod['symbol']}_{prod['expiryMonth']:02d}{prod['expiryDay']:02d}{str(prod['expiryYear'])[2:]}{opty}{str(prod['strikePrice']).replace('.0','')}"
                         else:
-                            ticker =  quote.get("Product").get('symbol')        
+                            ticker =  quote.get("Product").get('symbol')
                         resp[ticker] = {
                             'symbol' : ticker,
                             'description': quote.get("All").get("symbolDescription"),
-                            'askPrice': quote.get("All").get("ask"),  
-                            'bidPrice': quote.get("All").get("bid"),    
+                            'askPrice': quote.get("All").get("ask"),
+                            'bidPrice': quote.get("All").get("bid"),
                             'quoteTimeInLong': quote.get("dateTimeUTC")*1000,
                             "status": quote['quoteStatus']
                             }
@@ -274,14 +274,14 @@ class eTrade(BaseBroker):
         return resp
 
     @retry_on_exception()
-    def get_order_info(self, order_id): 
+    def get_order_info(self, order_id):
         """ Get order info from order_id, mimicks the order_info from TDA API"""
         order_id = int(order_id)
-        orders = self.order_session.list_orders(self.accountIdKey, resp_format='json')        
+        orders = self.order_session.list_orders(self.accountIdKey, resp_format='json')
         for order in orders['OrdersResponse']['Order']:
             if order['orderId'] == order_id:
                 order_status = order['OrderDetail'][0]['status'].upper().replace('EXECUTED','FILLED').replace('OPEN','WORKING')
-                order_info = self.format_order(order)         
+                order_info = self.format_order(order)
                 return order_status, order_info
         return None, None
 
@@ -312,25 +312,25 @@ class eTrade(BaseBroker):
                 'instrument':{'symbol':order['OrderDetail'][0]['Instrument'][0]['Product']['symbol']},
                 'instruction': order['OrderDetail'][0]['Instrument'][0]['orderAction'],
                 'quantity': order['OrderDetail'][0]['Instrument'][0]['filledQuantity'],
-            }]             
-        }    
+            }]
+        }
         return order_info
 
     @retry_on_exception()
-    def send_order(self, new_order:dict):        
+    def send_order(self, new_order:dict):
         order_response =  self.order_session.place_equity_order(
             resp_format="xml",
             accountId = self.accountId,
             accountIdKey = self.accountIdKey,
             **new_order)
-        
+
         order_id = int(order_response['PlaceOrderResponse']['OrderIds']['orderId'])
         _, ord_inf = self.get_order_info(order_id)
-        
+
         order_response['quantity'] =  int(order_response['PlaceOrderResponse']['Order']['Instrument']['quantity']),
-        order_response.update(ord_inf) 
+        order_response.update(ord_inf)
         return order_response, order_id
-    
+
     @retry_on_exception()
     def cancel_order(self, order_id:int):
         time.sleep(2)  # sleep as order status change is not instant
@@ -348,7 +348,7 @@ class eTrade(BaseBroker):
         elif action == "STO":
             kwargs['orderAction'] = "SELL_SHORT"
         if len(Symbol.split("_"))>1:
-            Symbol = self.format_option(Symbol)            
+            Symbol = self.format_option(Symbol)
             symbol, year, month, day, optype, strike = Symbol.split(":")
             kwargs['symbol'] = symbol
             kwargs['expiryDate'] = f"{year}-{month}-{day}"
@@ -361,9 +361,9 @@ class eTrade(BaseBroker):
                 kwargs['orderAction'] = 'SELL_OPEN'
         kwargs['clientOrderId'] = str(random.randint(1000000000, 9999999999))
         kwargs['priceType'] = 'LIMIT'
-        kwargs['limitPrice'] = price    
+        kwargs['limitPrice'] = price
         kwargs['allOrNone'] = False
-        kwargs['quantity'] = Qty       
+        kwargs['quantity'] = Qty
         kwargs['orderTerm'] = "GOOD_FOR_DAY"
         kwargs['marketSession'] = 'REGULAR'
         return kwargs
@@ -377,7 +377,7 @@ class eTrade(BaseBroker):
         elif action == "STO":
             kwargs['orderAction'] = "BUY_TO_COVER"
         if len(Symbol.split("_"))>1:
-            Symbol = self.format_option(Symbol)            
+            Symbol = self.format_option(Symbol)
             symbol, year, month, day, optype, strike = Symbol.split(":")
             kwargs['symbol'] = symbol
             kwargs['expiryDate'] = f"{year}-{month}-{day}"
@@ -393,7 +393,7 @@ class eTrade(BaseBroker):
         kwargs['limitPrice'] = PT
         kwargs['stopPrice'] = SL
         kwargs['allOrNone'] = False
-        kwargs['quantity'] = Qty       
+        kwargs['quantity'] = Qty
         kwargs['orderTerm'] = "GOOD_UNTIL_CANCEL"
         kwargs['marketSession'] = 'REGULAR'
         return kwargs
@@ -407,7 +407,7 @@ class eTrade(BaseBroker):
         elif action == "STO":
             kwargs['orderAction'] = "BUY_TO_COVER"
         if len(Symbol.split("_"))>1:
-            Symbol = self.format_option(Symbol)            
+            Symbol = self.format_option(Symbol)
             symbol, year, month, day, optype, strike = Symbol.split(":")
             kwargs['symbol'] = symbol
             kwargs['expiryDate'] = f"{year}-{month}-{day}"
@@ -422,7 +422,7 @@ class eTrade(BaseBroker):
         kwargs['priceType'] = 'LIMIT'
         kwargs['limitPrice'] = price
         kwargs['allOrNone'] = False
-        kwargs['quantity'] = int(Qty)       
+        kwargs['quantity'] = int(Qty)
         kwargs['orderTerm'] = "GOOD_UNTIL_CANCEL"
         kwargs['marketSession'] = 'REGULAR'
         return kwargs
@@ -436,7 +436,7 @@ class eTrade(BaseBroker):
         elif action == "STO":
             kwargs['orderAction'] = "BUY_TO_COVER"
         if len(Symbol.split("_"))>1:
-            Symbol = self.format_option(Symbol)            
+            Symbol = self.format_option(Symbol)
             symbol, year, month, day, optype, strike = Symbol.split(":")
             kwargs['symbol'] = symbol
             kwargs['expiryDate'] = f"{year}-{month}-{day}"
@@ -451,7 +451,7 @@ class eTrade(BaseBroker):
         kwargs['priceType'] = 'STOP'
         kwargs['stopPrice'] = SL
         kwargs['allOrNone'] = False
-        kwargs['quantity'] = Qty       
+        kwargs['quantity'] = Qty
         kwargs['orderTerm'] = "GOOD_UNTIL_CANCEL"
         kwargs['marketSession'] = 'REGULAR'
         return kwargs
@@ -468,9 +468,9 @@ class eTrade(BaseBroker):
             kwargs['orderAction'] = "BUY"
         elif action == "BTC":
             kwargs['orderAction'] = "BUY_TO_CLOSE"
-            
+
         if len(Symbol.split("_"))>1:
-            Symbol = self.format_option(Symbol)            
+            Symbol = self.format_option(Symbol)
             symbol, year, month, day, optype, strike = Symbol.split(":")
             kwargs['symbol'] = symbol
             kwargs['expiryDate'] = f"{year}-{month}-{day}"
@@ -484,13 +484,13 @@ class eTrade(BaseBroker):
             elif action == "BTO":
                 kwargs['orderAction'] = "BUY_OPEN"
             elif action == "BTC":
-                kwargs['orderAction'] = "BUY_CLOSE" 
+                kwargs['orderAction'] = "BUY_CLOSE"
 
         kwargs['clientOrderId'] = str(random.randint(1000000000, 9999999999))
         kwargs['priceType'] = 'TRAILING_STOP_CNST'
         kwargs['stopPrice'] = trail_stop_const
         kwargs['allOrNone'] = False
-        kwargs['quantity'] = Qty       
+        kwargs['quantity'] = Qty
         kwargs['orderTerm'] = "GOOD_UNTIL_CANCEL"
         kwargs['marketSession'] = 'REGULAR'
         return kwargs
@@ -519,7 +519,7 @@ if 0:
     order = rt.make_Lim_SL_order('VERB', 1,  1.8, 1.5)
     order_response, order_id = rt.send_order(order)
     res = rt.cancel_order(order_id)
-    
+
     order = rt.make_STC_SL_trailstop('VERB', 1, 40)
     order_response, order_id = rt.send_order(order)
     res = rt.cancel_order(order_id)
