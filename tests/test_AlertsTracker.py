788f537571e45b13f05f7e97de529f7234c0dd15
Nadir Ali Khan <124476721+NadirAliOfficial@users.noreply.github.com>
2026-07-23 16:14:31 +0500

    fix: remove trailing whitespace

--- tests/test_AlertsTracker.py
+++ tests/test_AlertsTracker.py
@@ -15,24 +15,24 @@ class TestAlertsTracker(unittest.TestCase):
         if os.path.exists(self.tracker_portfolio_fname):
             os.remove(self.tracker_portfolio_fname)
 
-        
+
     def test_sto(self):
         self.prepare()
         tracker = AlertsTracker(brokerage=None,
                                 portfolio_fname=self.tracker_portfolio_fname,
                                 dir_quotes=root_dir+'/data/live_quotes'
                                 )
-        
+
         buy = "STO 3  AAPL 100c 8/5 @1.5"
         sell = "BTC 3  AAPL 100c 8/5 @1"
         pars, order =  parse_trade_alert(buy)
         order["Trader"] = 'test'
-        msg_b = tracker.trade_alert(order, live_alert=False, channel=None)        
+        msg_b = tracker.trade_alert(order, live_alert=False, channel=None)
         pars, order =  parse_trade_alert(sell)
         order["Trader"] = 'test'
         order['Date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         msg_s = tracker.trade_alert(order, live_alert=False, channel=None)
-        
+
         trade = tracker.portfolio.loc[0]
         self.assertTrue(round(trade['PnL'],2) == 33.33)
         self.assertTrue(round(trade['PnL$']) == 150)
